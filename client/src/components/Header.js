c440d907995d7cf2b0abe8e0b2d9da6c85ed4f6b
Zoey Kim <gyulikimwork@gmail.com>
2023-09-09 00:35:07 +0000

    menu-icon css style

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import MenuItems from './MenuItems';
 import './Header.css';
 import { BsList } from 'react-icons/bs';
@@ -6,22 +6,10 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   const [open, setOpen] = useState(false);
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   const handleToggle = () => {
     setOpen(!open);
   };
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
 
   return (
     <div className="header">
@@ -34,26 +22,19 @@ const Header = () => {
           </h2>
         </div>
         <div className="menu">
-          {windowWidth > 744 ? (
-            <ul className="menu-horizontal">
-              <MenuItems active="horizontal" />
-            </ul>
+          <ul className="menu-horizontal">
+            <MenuItems active="horizontal" />
+          </ul>
+          {!open ? (
+            <BsList className="bslist-icon" onClick={handleToggle} />
           ) : (
-            <>
-              {!open ? (
-                <div className="menu-icon" onClick={handleToggle}>
-                  <BsList className="bslist-icon" />
-                </div>
-              ) : (
-                <div className="shade" onClick={handleToggle}></div>
-              )}
-              <ul
-                className={`menu-vertical ${open ? 'open' : ''}`}
-                onClick={handleToggle}>
-                <MenuItems active="vertical" />
-              </ul>
-            </>
+            <div className="shade" onClick={handleToggle}></div>
           )}
+          <ul
+            className={`menu-vertical ${open ? 'open' : ''}`}
+            onClick={handleToggle}>
+            <MenuItems active="vertical" />
+          </ul>
         </div>
       </div>
     </div>
